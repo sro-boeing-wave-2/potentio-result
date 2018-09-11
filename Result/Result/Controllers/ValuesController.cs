833a0a314eb07e655e60fd3a4ba4da764f2e18f5
Yaiphabee <ksh.yaipha@gmail.com>
2018-09-11 11:24:21 +0530

    Values controller

---
+++ Result/Result/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Result.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<String> GetValues()
+        {
+            return new List<string> { "Value10", "Value20" };
+        }
+    }
+}
