28f6c62012cc8338b237ee93cad55a5787c8e842
Yaiphabee <ksh.yaipha@gmail.com>
2018-09-24 15:12:48 +0530

    commented testing

--- Result/ResultViewAPI.Testing/MockUserResult.cs
+++ Result/ResultViewAPI.Testing/MockUserResult.cs
@@ -1,5 +1,69 @@
-<<<<<<< HEAD
-﻿//using System;
+
+//﻿//using System;
+////using System.Collections.Generic;
+////using System.Text;
+////using Result.Models;
+////using System.Threading.Tasks;
+
+////namespace ResultViewAPI.Testing
+////{
+////    class MockUserResult
+////    {
+////        public async Task<UserResult> GetTestResultData()
+////        {
+////            var UserResult = new UserResult()
+////            {
+////                UserId = 10,
+////                DomainName = "Java",
+////                Scores = new List<double>() { 50, 50, 50 },
+////                AverageScore = 50,
+
+////            };
+////            return await Task.FromResult(UserResult);
+
+////        }
+////        public async Task<UserQuizDetail> GetQuizEntry()
+////        {
+////            QuestionAttempted qa = new QuestionAttempted();
+////            var QuizEntry = new UserQuizDetail()
+////            {
+////                UserId = 10,
+////                Domain = "Java",
+////                //Score = 50,
+
+////            };
+////            return await Task.FromResult(QuizEntry);
+
+////        }
+////        public async Task<List<UserResult>> GetTestResultListAsync()
+////        {
+////            var UserResults = new List<UserResult>
+////            {
+////                new UserResult()
+////                {
+////                    UserId = 10,
+////                    DomainName = "Java",
+////                    Scores  = new List<double>(){50,50,50},
+////                    AverageScore = 50,
+
+////                },
+////                new UserResult()
+////                {
+
+////                    UserId = 16,
+////                    DomainName = "C",
+////                    AverageScore = 60,
+////                    Scores  = new List<double>(){60,60,60},
+
+
+////                }
+////            };
+////            return await Task.FromResult(UserResults);
+////        }
+////    }
+////}
+
+
 //using System.Collections.Generic;
 //using System.Text;
 //using Result.Models;
@@ -9,121 +73,56 @@
 //{
 //    class MockUserResult
 //    {
-//        public async Task<UserResult> GetTestResultData()
-//        {
-//            var UserResult = new UserResult()
-//            {
-//                UserId = 10,
-//                DomainName = "Java",
-//                Scores = new List<double>() { 50, 50, 50 },
-//                AverageScore = 50,
+//        // public async Task<UserResult> GetTestResultData()
+//        // {
+//        //     var UserResult = new UserResult()
+//        //     {
+//        //         UserId = 10,
+//        //         DomainName = "Java",
+//        //         Scores = new List<double>() { 50, 50, 50 },
+//        //         AverageScore = 50,
 
-//            };
-//            return await Task.FromResult(UserResult);
+//        //     };
+//        //     return await Task.FromResult(UserResult);
 
-//        }
-//        public async Task<UserQuizDetail> GetQuizEntry()
-//        {
-//            QuestionAttempted qa = new QuestionAttempted();
-//            var QuizEntry = new UserQuizDetail()
-//            {
-//                UserId = 10,
-//                Domain = "Java",
-//                //Score = 50,
+//        // }
+//        // public async Task<UserQuizDetail> GetQuizEntry()
+//        // {
+//        //     QuestionAttempted qa = new QuestionAttempted();
+//        //     var QuizEntry = new UserQuizDetail()
+//        //     {
+//        //         UserId = 10,
+//        //         Domain = "Java",
+//        //         //Score = 50,
 
-//            };
-//            return await Task.FromResult(QuizEntry);
+//        //     };
+//        //     return await Task.FromResult(QuizEntry);
 
-//        }
-//        public async Task<List<UserResult>> GetTestResultListAsync()
-//        {
-//            var UserResults = new List<UserResult>
-//            {
-//                new UserResult()
-//                {
-//                    UserId = 10,
-//                    DomainName = "Java",
-//                    Scores  = new List<double>(){50,50,50},
-//                    AverageScore = 50,
+//        // }
+//        // public async Task<List<UserResult>> GetTestResultListAsync()
+//        // {
+//        //     var UserResults = new List<UserResult>
+//        //     {
+//        //         new UserResult()
+//        //         {
+//        //             UserId = 10,
+//        //             DomainName = "Java",
+//        //             Scores  = new List<double>(){50,50,50},
+//        //             AverageScore = 50,
 
-//                },
-//                new UserResult()
-//                {
+//        //         },
+//        //         new UserResult()
+//        //         {
 
-//                    UserId = 16,
-//                    DomainName = "C",
-//                    AverageScore = 60,
-//                    Scores  = new List<double>(){60,60,60},
+//        //             UserId = 16,
+//        //             DomainName = "C",
+//        //             AverageScore = 60,
+//        //             Scores  = new List<double>(){60,60,60},
 
 
-//                }
-//            };
-//            return await Task.FromResult(UserResults);
-//        }
+//        //         }
+//        //     };
+//        //     return await Task.FromResult(UserResults);
+//        // }
 //    }
 //}
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Result.Models;
-using System.Threading.Tasks;
-
-namespace ResultViewAPI.Testing
-{
-    class MockUserResult
-    {
-        // public async Task<UserResult> GetTestResultData()
-        // {
-        //     var UserResult = new UserResult()
-        //     {
-        //         UserId = 10,
-        //         DomainName = "Java",
-        //         Scores = new List<double>() { 50, 50, 50 },
-        //         AverageScore = 50,
-
-        //     };
-        //     return await Task.FromResult(UserResult);
-
-        // }
-        // public async Task<UserQuizDetail> GetQuizEntry()
-        // {
-        //     QuestionAttempted qa = new QuestionAttempted();
-        //     var QuizEntry = new UserQuizDetail()
-        //     {
-        //         UserId = 10,
-        //         Domain = "Java",
-        //         //Score = 50,
-
-        //     };
-        //     return await Task.FromResult(QuizEntry);
-
-        // }
-        // public async Task<List<UserResult>> GetTestResultListAsync()
-        // {
-        //     var UserResults = new List<UserResult>
-        //     {
-        //         new UserResult()
-        //         {
-        //             UserId = 10,
-        //             DomainName = "Java",
-        //             Scores  = new List<double>(){50,50,50},
-        //             AverageScore = 50,
-
-        //         },
-        //         new UserResult()
-        //         {
-
-        //             UserId = 16,
-        //             DomainName = "C",
-        //             AverageScore = 60,
-        //             Scores  = new List<double>(){60,60,60},
-
-
-        //         }
-        //     };
-        //     return await Task.FromResult(UserResults);
-        // }
-    }
-}
->>>>>>> 42ff5253791c881b363d874199c9a09bcac79d90
